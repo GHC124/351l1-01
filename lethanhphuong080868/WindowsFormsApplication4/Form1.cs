da68d359724d13e256237fcb282840fb9a28e1e8
linhxinh0104@gmail.com <linhxinh0104@gmail.com@f3903599-4fb1-e027-8a45-a858ab32afe9>
2010-11-29 02:52:51 +0000

    lethanhphuong

---
+++ lethanhphuong080868/WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+      
+
+        private void btnthuchien_Click(object sender, EventArgs e)
+        {
+            if (cbbtoan.Text == "+")
+                txtKQ.Text = (Convert.ToInt32(txtnumber1.Text) + Convert.ToInt32(txtnumber2.Text)).ToString();
+            else if (cbbtoan.Text == "-")
+                txtKQ.Text = (Convert.ToInt32(txtnumber1.Text) - Convert.ToInt32(txtnumber2.Text)).ToString();
+            else if (cbbtoan.Text == "*")
+                txtKQ.Text = (Convert.ToDouble(txtnumber1.Text) * Convert.ToDouble(txtnumber2.Text)).ToString();
+            else if (cbbtoan.Text == "/")
+                txtKQ.Text=(Convert.ToDouble(txtnumber1.Text) / Convert.ToDouble(txtnumber2.Text)).ToString();
+        }
+
+        private void btnthoat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnclear_Click(object sender, EventArgs e)
+        {
+            txtKQ.Clear();
+            txtnumber1.Text = "";
+            txtnumber2.Text = "";
+            cbbtoan.Text = "";
+
+        }
+
+        
+    }
+}
