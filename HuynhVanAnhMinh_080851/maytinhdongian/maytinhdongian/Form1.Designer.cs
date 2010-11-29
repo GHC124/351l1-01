fa9e41e8a52ff1956c2df2c3b8346d2443fd3f58
H.V.A.Minh <H.V.A.Minh@f3903599-4fb1-e027-8a45-a858ab32afe9>
2010-11-29 15:06:08 +0000

    HuynhVanAnhMinh_080851

---
+++ HuynhVanAnhMinh_080851/maytinhdongian/maytinhdongian/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace maytinhdongian
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toantu1 = new System.Windows.Forms.TextBox();
+            this.toantu2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtketqua = new System.Windows.Forms.TextBox();
+            this.lblketqua = new System.Windows.Forms.Label();
+            this.rdoCong = new System.Windows.Forms.RadioButton();
+            this.rdoTru = new System.Windows.Forms.RadioButton();
+            this.rdoChia = new System.Windows.Forms.RadioButton();
+            this.rdoNhan = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toantu1
+            // 
+            this.toantu1.Location = new System.Drawing.Point(121, 19);
+            this.toantu1.Name = "toantu1";
+            this.toantu1.Size = new System.Drawing.Size(100, 20);
+            this.toantu1.TabIndex = 0;
+            this.toantu1.TextChanged += new System.EventHandler(this.toantu1_TextChanged);
+            // 
+            // toantu2
+            // 
+            this.toantu2.Location = new System.Drawing.Point(121, 65);
+            this.toantu2.Name = "toantu2";
+            this.toantu2.Size = new System.Drawing.Size(100, 20);
+            this.toantu2.TabIndex = 2;
+            this.toantu2.TextChanged += new System.EventHandler(this.toantu2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 14);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Toán tử 1:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 14);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Toán tử 2:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(186, 309);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 25);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Tính toán";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(383, 309);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 25);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Thoát";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 14);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Phép toán:";
+            // 
+            // txtketqua
+            // 
+            this.txtketqua.Location = new System.Drawing.Point(128, 233);
+            this.txtketqua.Name = "txtketqua";
+            this.txtketqua.ReadOnly = true;
+            this.txtketqua.Size = new System.Drawing.Size(100, 20);
+            this.txtketqua.TabIndex = 9;
+            // 
+            // lblketqua
+            // 
+            this.lblketqua.AutoSize = true;
+            this.lblketqua.Location = new System.Drawing.Point(40, 236);
+            this.lblketqua.Name = "lblketqua";
+            this.lblketqua.Size = new System.Drawing.Size(47, 14);
+            this.lblketqua.TabIndex = 5;
+            this.lblketqua.Text = "Kết quả:";
+            this.lblketqua.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // rdoCong
+            // 
+            this.rdoCong.AutoSize = true;
+            this.rdoCong.Checked = true;
+            this.rdoCong.Location = new System.Drawing.Point(7, 37);
+            this.rdoCong.Name = "rdoCong";
+            this.rdoCong.Size = new System.Drawing.Size(49, 18);
+            this.rdoCong.TabIndex = 10;
+            this.rdoCong.TabStop = true;
+            this.rdoCong.Text = "cong";
+            this.rdoCong.UseVisualStyleBackColor = true;
+            // 
+            // rdoTru
+            // 
+            this.rdoTru.AutoSize = true;
+            this.rdoTru.Location = new System.Drawing.Point(116, 37);
+            this.rdoTru.Name = "rdoTru";
+            this.rdoTru.Size = new System.Drawing.Size(38, 18);
+            this.rdoTru.TabIndex = 10;
+            this.rdoTru.TabStop = true;
+            this.rdoTru.Text = "tru";
+            this.rdoTru.UseVisualStyleBackColor = true;
+            // 
+            // rdoChia
+            // 
+            this.rdoChia.AutoSize = true;
+            this.rdoChia.Location = new System.Drawing.Point(333, 37);
+            this.rdoChia.Name = "rdoChia";
+            this.rdoChia.Size = new System.Drawing.Size(45, 18);
+            this.rdoChia.TabIndex = 10;
+            this.rdoChia.TabStop = true;
+            this.rdoChia.Text = "chia";
+            this.rdoChia.UseVisualStyleBackColor = true;
+            // 
+            // rdoNhan
+            // 
+            this.rdoNhan.AutoSize = true;
+            this.rdoNhan.Location = new System.Drawing.Point(225, 37);
+            this.rdoNhan.Name = "rdoNhan";
+            this.rdoNhan.Size = new System.Drawing.Size(49, 18);
+            this.rdoNhan.TabIndex = 10;
+            this.rdoNhan.TabStop = true;
+            this.rdoNhan.Text = "nhan";
+            this.rdoNhan.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdoTru);
+            this.groupBox1.Controls.Add(this.rdoNhan);
+            this.groupBox1.Controls.Add(this.rdoCong);
+            this.groupBox1.Controls.Add(this.rdoChia);
+            this.groupBox1.Location = new System.Drawing.Point(121, 111);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(425, 90);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(419, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 43);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Caculator";
+            this.label4.Click += new System.EventHandler(this.label4_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 393);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtketqua);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblketqua);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toantu2);
+            this.Controls.Add(this.toantu1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox toantu1;
+        private System.Windows.Forms.TextBox toantu2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtketqua;
+        private System.Windows.Forms.Label lblketqua;
+        private System.Windows.Forms.RadioButton rdoCong;
+        private System.Windows.Forms.RadioButton rdoTru;
+        private System.Windows.Forms.RadioButton rdoChia;
+        private System.Windows.Forms.RadioButton rdoNhan;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
