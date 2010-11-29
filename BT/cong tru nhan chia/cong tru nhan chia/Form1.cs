8bfb771254c1088a47eac7afb9768eb2cbafa1a2
CanIhelpU1990@gmail.com <CanIhelpU1990@gmail.com@f3903599-4fb1-e027-8a45-a858ab32afe9>
2010-11-29 02:57:17 +0000

    hoang thanh tung 080905

---
+++ BT/cong tru nhan chia/cong tru nhan chia/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using csUnit;
+
+namespace cong_tru_nhan_chia
+{
+    [TestFixture]
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        [Test]
+        public bool check(string a)
+        {
+            for (int i = 0; i < a.Length; i++)
+                if (!char.IsDigit(a[i])&&a[i]!='.')
+                    return false;
+            return true;
+        
+        }
+        [Test]
+        private void r_cong_CheckedChanged(object sender, EventArgs e)
+        {
+            if (r_cong.Checked == true)
+            {
+                if (check(txt_so1.Text) == false || check(txt_so2.Text) == false)
+                    MessageBox.Show("ban vui long nhap so ");
+                else
+                {
+                    if (txt_so1.Text == "" || txt_so2.Text == "")
+                        MessageBox.Show("ban chua nhap so");
+                    else
+                        txt_ketqua.Text = Convert.ToString(Convert.ToDouble(txt_so1.Text) + Convert.ToDouble(txt_so2.Text));
+                }
+            }
+        }
+
+        private void r_chia_CheckedChanged(object sender, EventArgs e)
+        {
+            if (r_chia.Checked == true)
+            {
+                if (check(txt_so1.Text) == false || check(txt_so2.Text) == false)
+                    MessageBox.Show("ban vui long nhap so ");
+                else
+                {
+                    if (txt_so1.Text == "" || txt_so2.Text == "")
+                        MessageBox.Show("ban chua nhap so");
+                    else
+                        if (Convert.ToDouble(txt_so2.Text) == 0)
+                        {
+                            if (r_chia.Checked == true)
+                                MessageBox.Show("ko the tinh dc");
+                        }
+                        else
+                            txt_ketqua.Text = Convert.ToString(Convert.ToDouble(txt_so1.Text) / Convert.ToDouble(txt_so2.Text));
+
+
+                }
+            }
+        }
+        [Test]
+        private void r_tru_CheckedChanged(object sender, EventArgs e)
+        {
+            if (r_tru.Checked == true)
+            {
+                if (check(txt_so1.Text) == false || check(txt_so2.Text) == false)
+                    MessageBox.Show("ban vui long nhap so ");
+                else
+                {
+                    if (txt_so1.Text == "" || txt_so2.Text == "")
+                        MessageBox.Show("ban chua nhap so");
+                    else
+                        txt_ketqua.Text = Convert.ToString(Convert.ToDouble(txt_so1.Text) - Convert.ToDouble(txt_so2.Text));
+                }
+            }
+        }
+
+        private void r_nhan_CheckedChanged(object sender, EventArgs e)
+        {
+            if (r_nhan.Checked == true)
+            {
+                if (check(txt_so1.Text) == false || check(txt_so2.Text) == false)
+                    MessageBox.Show("ban vui long nhap so ");
+                else
+                {
+                    if (txt_so1.Text == "" || txt_so2.Text == "")
+                        MessageBox.Show("ban chua nhap so");
+                    else
+                        txt_ketqua.Text = Convert.ToString(Convert.ToDouble(txt_so1.Text) * Convert.ToDouble(txt_so2.Text));
+                }
+            }
+        }
+
+        private void txt_so1_TextChanged(object sender, EventArgs e)
+        {
+
+            if (check(txt_so1.Text) == false )
+                errorProvider1.SetError(txt_so1,"ban vui long nhap so ");
+            else
+            {
+                if (txt_so1.Text == "" )
+                    errorProvider1.SetError(txt_so1,"ban chua nhap so");
+                else
+                    errorProvider1.Clear();
+            }
+        }
+
+        private void txt_so2_TextChanged(object sender, EventArgs e)
+        {
+            if (check(txt_so2.Text) == false )
+                errorProvider1.SetError(txt_so2, "ban vui long nhap so ");
+            else
+            {
+                if (txt_so2.Text == "")
+                    errorProvider1.SetError(txt_so2, "ban chua nhap so");
+                else
+                    errorProvider1.Clear();
+            }
+        }
+    }
+}
